2068858f91bb3c487385746fbfb9f14e4e9a7ddf
harusys <haruki.sys@gmail.com>
2023-01-22 09:22:11 +0000

    [AC] abs abc085_c

---
+++ atcoder.jp/abs/abc085_c/Main.py
@@ -0,0 +1,8 @@
+N, total = map(int, input().split())
+
+for i in range(N + 1):
+    for j in range(N - i + 1):
+        if 10000*i + 5000*j + 1000*(N-i-j) == total:
+            print(i, j, N-i-j)
+            exit()
+print("-1 -1 -1")
