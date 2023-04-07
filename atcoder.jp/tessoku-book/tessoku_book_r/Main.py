762625dda33f3288e9333677df3d8fb7fc98fcf6
harusys <haruki.sys@gmail.com>
2023-04-07 14:33:51 +0000

    [AC] tessoku-book tessoku_book_r

---
+++ atcoder.jp/tessoku-book/tessoku_book_r/Main.py
@@ -0,0 +1,19 @@
+N, S = map(int, input().split())
+A = list(map(int, input().split()))
+
+dp = [ [ None ] * (S + 1) for i in range(N + 1) ]
+dp[0][0] = True
+
+for i in range(1, S+1):
+  dp[0][i] = False
+
+for i in range(1, N+1):
+  for j in range(S+1):
+    # カードを選ばない場合
+    dp[i][j] = dp[i-1][j]
+    # カードを選ぶ場合
+    if j - A[i-1] >= 0:
+      if dp[i-1][j-A[i-1]]:
+        dp[i][j] = True
+
+print("Yes" if dp[N][S] else "No")
