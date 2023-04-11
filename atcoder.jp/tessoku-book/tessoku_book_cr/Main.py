e4201d9f34ef38a796b48a1386f336c98fa7ee7b
harusys <haruki.sys@gmail.com>
2023-04-11 14:34:07 +0000

    [AC] tessoku-book tessoku_book_cr

---
+++ atcoder.jp/tessoku-book/tessoku_book_cr/Main.py
@@ -0,0 +1,20 @@
+N, W = map(int, input().split())
+w = [ None ] * (N + 1)
+v = [ None ] * (N + 1)
+for i in range(1, N+1):
+  w[i], v[i] = map(int, input().split())
+
+dp = [ [ 10 ** 15 ] * 100001 for _ in range(N + 1) ]
+dp[0][0] = 0
+for i in range(1, N+1):
+  for j in range(100001):
+    if j < v[i]:
+      dp[i][j] = dp[i-1][j]
+    if j >= v[i]:
+      dp[i][j] = min(dp[i-1][j], dp[i-1][j-v[i]]+w[i])
+
+ans = 0
+for i in range(100001):
+	if dp[N][i] <= W:
+		ans = i
+print(ans)
